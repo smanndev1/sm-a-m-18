2e3332b4b3208b2526ac45e6a0fee18ff1c5c66c
smanndev1 <smanndev1@gmail.com>
2024-10-10 18:25:34 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,42 @@
-import { Component, ViewChild, afterNextRender, inject, Injector } from '@angular/core';
+import { Component, ViewChild, afterNextRender, inject, Injector, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatInputModule} from '@angular/material/input';
-import {FormsModule} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormArray} from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, MatInputModule, RouterOutlet],
+  imports: [FormsModule, MatInputModule, ReactiveFormsModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+ 
+  formGroup = this.fb.group({
 
- letters = ['a', 'b', 'c' , '', 'p' ]
+  });
 
+  word = ['a', 'b', 'c' , '', 'p' ]
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.createSpaces();
+  }
+
+ 
+  createSpaces() {
+    this.word.forEach((element: String, index: number)  => {
+      this.formGroup.addControl('control'+ (index + 1), new FormControl('10'))
+    });
+  }
+
+
+ checkAnswer() {
+  
+
+}
 }
